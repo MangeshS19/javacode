cb6ce9c166070a2565e7143e24f1c83e61bc7918
MangeshS19 <112706662+MangeshS19@users.noreply.github.com>
2024-12-08 00:49:44 +0530

    Update printOneToTen.java
    code updated

--- printOneToTen.java
+++ printOneToTen.java
@@ -5,7 +5,7 @@ public class printOneToTen {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for(int i =0; i<=1 ; i--)
+		for(int i =1; i<=10 ; i++)
 		{
 			System.out.println(i);
 		}
